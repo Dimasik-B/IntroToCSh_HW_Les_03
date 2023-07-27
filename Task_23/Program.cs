87a2be6c22a6fd9427a944b11e39d8674cde1889
Dima Bykadorov <dimka.b@live.com>
2023-07-27 22:01:08 +0300

    add Task 23

---
+++ Task_23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int count = 1;
+string result = "";
+while (count <= number){
+    result += Math.Pow(count, 3).ToString() + ", ";
+    count++;
+}
+Console.WriteLine(result.Remove(result.Length-2));
